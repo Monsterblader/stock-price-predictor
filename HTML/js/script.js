00fc885bae9f53ffdf94989c4874f57c3100216b
Monsterblader <Monsterblader@gmail.com>
2020-11-30 21:20:30 -0800

    Sets up presentation.

---
+++ HTML/js/script.js
@@ -0,0 +1,113 @@
+const resizeViews = {
+  active: {
+    'border-radius': '0px',
+    height: '1080px',
+    left: '0px',
+    top: '0px',
+    width: '1920px',
+  },
+
+  box: {
+    'background-color': '#fff',
+    'border-radius': '5px',
+    height: '108px',
+    position: 'absolute',
+    width: '192px',
+  },
+
+  one: {
+    left: '236px',
+    top: '161px',
+  },
+
+  two: {
+    left: '453px',
+    top: '353px',
+  },
+
+  three: {
+    left: '642px',
+    top: '542px',
+  },
+
+  four: {
+    left: '832px',
+    top: '275px',
+  },
+
+  five: {
+    left: '1083px',
+    top: '117px',
+  },
+
+  expand: source => {
+    const target = d3.select('.box.' + source);
+    const cover = d3.select('.box.' + source + ' .cover');
+
+    target.style('z-index', 10);
+    // zoom cover to left corner
+    target.classed('active', true)
+      .transition()
+      .duration(1000)
+      .ease(d3.easeExpInOut)
+      .style('border-radius', '0px')
+      .style('left', '0px')
+      .style('top', '0px')
+      .style('width', '1920px')
+      .style('height', '1080px')
+      .style('opacity', 1);
+
+    cover.transition()
+      .duration(1000)
+      .ease(d3.easeExpInOut)
+      .style('left', '0%')
+      .style('top', '0%')
+      .style('font-size', '0px');
+  },
+
+  restore: source => {
+    d3.select('.active .cover')
+      .transition()
+      .duration(1000)
+      .ease(d3.easeExpInOut)
+      .style('left', '50%')
+      .style('top', '50%')
+      .style('font-size', '24px');
+
+    d3.select('.active')
+      .classed('active', false)
+      .transition()
+      .duration(1000)
+      .ease(d3.easeExpInOut)
+      .style('border-radius', '10px')
+      .style('left', resizeViews[source].left)
+      .style('top', resizeViews[source].top)
+      .style('width', '192px')
+      .style('height', '108px')
+      .style('opacity', 0.9)
+      .style('z-index', 1);
+  },
+
+  moveView: (left = 0, top = 0) => {
+    d3.select('.map')
+      .transition()
+      .duration(1000)
+      .style('left', left + "px")
+      .style('top', top + "px");
+  },
+}
+
+const checkKey = e => {
+  const err = e || window.event;
+  const getBox = () => {
+    let className = '';
+
+    return d3.select('.active')._groups[0][0].classList[1];
+  }
+
+  if (e.keyCode == '27') {
+    resizeViews.restore(getBox());
+  }
+}
+
+document.onkeyup = checkKey;
---
+++ HTML/server.py
@@ -0,0 +1,43 @@
+from flask import Flask, request, render_template
+from flask.helpers import send_from_directory
+
+app = Flask(__name__)
+
+
+@app.route('/')  # the site to route to, index/main in this case
+def hello_world():
+    return send_from_directory('.', 'index.html')
+
+
+@app.route('/favicon.ico')  # the site to route to, index/main in this case
+def send_favicon():
+    return send_from_directory('./', 'favicon.ico')
+
+
+@app.route('/css/<path:path>')
+def send_css(path):
+    return send_from_directory('css', path)
+
+
+@app.route('/js/<path:path>')
+def send_js(path):
+    return send_from_directory('js', path)
+
+
+@app.route('/images/<path:path>')
+def send_images(path):
+    return send_from_directory('images', path)
+
+
+@app.route('/pages/<path:path>')
+def send_pages(path):
+    return send_from_directory('pages', path)
+
+
+@app.route('/media/<path:path>')
+def send_media(path):
+    return send_from_directory('media', path)
+
+
+# This just gets flask running
+app.run(port=5000, debug=True)
